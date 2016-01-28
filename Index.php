178f1d0b36e5d46bc418e5e5a8cfb708e63802da
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-01-28 14:01:40 -0700

    Made a bar with a few of the pages needed but don't go anywhere yet

---
+++ Index.php
@@ -0,0 +1,18 @@
+<?php ?>
+
+<!DOCTYPE html>
+<title>My Website</title>
+    <h1 id="name">My Website</h1>
+<link rel="stylesheet" type="text/css" href="styleSheet.css">
+</head>
+<ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#product">Products</a></li>
+    <ul style="float:right;list-style-type:none;">
+        <li><a href="#about">About</a></li>
+        <li><a href="#cart">Your Items</a></li>
+    </ul>
+</ul>
+</html>
