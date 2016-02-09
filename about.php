b2c700725218c73bcc8d32058cfbdbe73138a333
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-02-09 14:08:53 -0700

    Changed href to nothing

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
     <h1>About Me</h1>
     <hr>
     <p>N3rds-R-Us is a website for all N3rdy G4mers who want to play video games like the pros. Don't be that guy (Or girl) that doesn't get gaming things because you think it is pointless because of how much money things are. These items are the best in town and feel, look, and smell great.</p>
-    <footer><p>Copyright 2016</p><a href="http://jacobstestsite.comli.com/" id="email1">My"hidden"email@gmail.com</a></footer>
+    <footer><p>Copyright 2016</p><a href="#" id="email1">My"hidden"email@gmail.com</a></footer>
 </head>
 </body>
 </html>
