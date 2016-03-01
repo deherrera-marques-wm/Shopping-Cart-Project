056953f517bada51bddf672810b82bc770857556
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-03-01 14:59:31 -0700

    work

--- Index.php
+++ Index.php
@@ -13,10 +13,7 @@
     <li><a href="about.php">About</a></li>
     <ul style="float:right;list-style-type:none;">
         <li><a href="create%20account.php">Create Account</a></li>
-        <li><?php echo $_SESSION['user'] -> email; ?>
-
-
-        </li>
+        <li><?php echo $_SESSION['email']; ?></li>
     </ul>
 </ul>
 
